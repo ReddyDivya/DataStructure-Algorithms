4387bd2b7bc8f1594d0f8cd286cb8f7e9508a797
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2022-10-16 13:40:47 +0530

    Add files via upload

--- CheckingSumPairZero.js
+++ CheckingSumPairZero.js
@@ -2,11 +2,13 @@
 #1 - Returning first pair of the sum which results in zero from the array.
 input - [-5,-4,-3,-2,0,2,4,6,8]
 output - [-4,4]
+It's a O(n^2) - quadratic Time Complexity which means we've used 1 loops to achieve this.
 */
 const getFirstSumPairZero = (array) => {
     for (let number of array) {
         console.log('Outer loop');
-        for (let i = 1; i < array.length; i++) {
+        for (let i = 1; i < array.length; i++) //always comparing from index 1 till end
+        {
             console.log('Inner loop ' + array[i]);
             if (number + array[i] === 0)
                 return [number, array[i]];
